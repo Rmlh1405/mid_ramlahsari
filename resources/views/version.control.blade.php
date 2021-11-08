16cbc9105e9ef5f044bfa6c432b663d8f51d93ff
LAPTOP-GIUH8V1K\LENOVO <rmlh1405@gmail.com>
2021-11-09 00:57:05 +0800

    selesai... RamlahSari_60900119046_B

---
+++ resources/views/version.control.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+Version control adalah sebuah sistem yang merekam perubahan-perubahan dari sebuah berkas atau
+sekumpulan berkas dari waktu ke waktu sehingga Anda dapat menilik kembali versi khusus suatu saat
+nanti.
+macam-macam version control
+1. Bazaar
+2. Subversion (SVN)
+3. Mercurial
+4. CVS
+5. RCS
+</body>
+</html>
